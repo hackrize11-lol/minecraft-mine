22b17dde543ca3abfda286525cef830b041cbdfc
hackrize11-lol <167661038+hackrize11-lol@users.noreply.github.com>
2024-06-15 19:47:07 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 
+const buttons = document.querySelectorAll('button');
+
+// Add event listener to each button
+buttons.forEach(button => {
+    button.addEventListener('click', function() {
+        alert(`Button ${button.id} clicked!`);
+    });
+});
