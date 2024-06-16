33fd8bcfbd8aa52d1596015d797b3217a54d7b08
hackrize11-lol <167661038+hackrize11-lol@users.noreply.github.com>
2024-06-15 20:05:29 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 // script.js
 
-// Select all elements with IDs starting with 'button'
+// Select all buttons
 const buttons = document.querySelectorAll('button');
 
 // Counter initialization
 let count = 0;
 const counterElement = document.getElementById('counter');
 
-// Update counter function
+
 function updateCounter() {
     count++;
-    counterElement.textContent = blocks mined:${count};
+    counterElement.textContent = `Blocks Mined: ${count}`;
 }
 
-// Add event listener to each button
+
 buttons.forEach(button => {
     button.addEventListener('click', function() {
-        // Call the updateCounter function when button is clicked
+       
         updateCounter();
-        
+
     });
 });
